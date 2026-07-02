3b3f9925cb3eebf222e4e989395b9c43b425ae6c
Alokkumar Gupta <alokkumar568g@gmail.com>
2026-07-02 15:44:38 +0530

    Initial commit

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,35 @@
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
+
+const Message = ({ message }) => {
+	const { authUser } = useAuthContext();
+	const { selectedConversation } = useConversation();
+	const fromMe = message.senderId === authUser._id;
+	const formattedTime = extractTime(message.createdAt);
+	const chatClassName = fromMe ? "chat-end" : "chat-start";
+	const profilePic = fromMe ? authUser.profilePicture : selectedConversation?.profilePicture;
+	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+
+	const shakeClass = message.shouldShake ? "shake" : "";
+
+	const effectiveProfilePic = (!profilePic || profilePic.includes("avatar.iran.liara.run"))
+		? `https://api.dicebear.com/9.x/micah/svg?seed=${fromMe ? authUser.username : selectedConversation?.username}`
+		: profilePic;
+
+	return (
+		<div className={`chat ${chatClassName}`}>
+			<div className='chat-image avatar'>
+				<div className='w-10 rounded-full'>
+					<img 
+						alt='Tailwind CSS chat bubble component' 
+						src={effectiveProfilePic} 
+					/>
+				</div>
+			</div>
+			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
+		</div>
+	);
+};
+export default Message;
---
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react'
+import Messages from './Messages'
+import MessageInput from './MessageInput'
+import { TiMessages, TiArrowBack } from 'react-icons/ti'
+import useConversation from '../../zustand/useConversation'
+import { useAuthContext } from '../../context/AuthContext'
+
+const MessageContainer = () => {
+    const {selectedConversation, setSelectedConversation} = useConversation();
+
+    useEffect(() => {
+        return () => {
+            // clear messages when conversation changes
+            setSelectedConversation(null);
+        }
+    }, [setSelectedConversation]);
+    
+  return (
+    <div className='md:min-w-[450px] flex flex-col h-full'>
+        {!selectedConversation ? <NoChatSelected/> : (<>
+            <div className='bg-slate-500 px-4 py-2 mb-2 flex items-center gap-2'>
+                <TiArrowBack className='md:hidden text-3xl cursor-pointer text-white' onClick={() => setSelectedConversation(null)} />
+                <span className='label-text'>To:</span><span className='text-gray-900 font-bold'>{selectedConversation.fullname}</span>
+            </div>
+            <Messages />
+            <MessageInput />
+        </>)}
+    </div>
+  )
+}
+
+export default MessageContainer;
+
+const NoChatSelected = () => {
+    const {authUser} = useAuthContext();
+    return (
+        <div className='flex items-center justify-center w-full h-full'>
+            <div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
+                <p>Welcome 👋 {authUser.fullname}</p>
+                <p>Select a chat to start a conversation</p>
+                <TiMessages className='text-3xl md:text-6xl text-center' />
+            </div>
+        </div>
+    )
+}
