e2963b14cae38297d6e0366cfa0463ac1db34fad
Alokkumar Gupta <alokkumar568g@gmail.com>
2026-07-12 00:21:57 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,12 +2,12 @@ import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: "15d"});
-    res.cookie("jwt", token, {
-        httpOnly: true, // prevent XSS attacks i.e., cross-site scripting
-        secure: process.env.NODE_ENV !== "development", // only set cookie in production
-        sameSite: "strict",
-        maxAge: 15 * 24 * 60 * 60 * 1000
-    });
+   res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+});
 }
 
 export default generateTokenAndSetCookie;
