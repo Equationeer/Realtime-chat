f6c8f508da37439018a2d132b586a4afa48026b6
Alokkumar Gupta <alokkumar568g@gmail.com>
2026-07-12 00:09:22 +0530

    Update useLogin.js

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -14,7 +14,7 @@ const useLogin = () => {
 
     setLoading(true);
     try {
-      const res = await fetch("/api/auth/login", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
